dc390d76b3456cc97d1e9454d9ae9461e1ba1dd1
azj7319 <568963700@qq.com>
2019-05-07 14:40:32 +0800

    新增设备地图展示

---
+++ src/components/teamacc/OverView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class OverView extends Component {
+    constructor(props){
+        super(props);
+    }
+    
+    render() { 
+        return (
+            <div className="OverView">
+                ss
+            </div>
+
+        )
+    }
+
+}
+
+export default OverView;
